9837d43302c1eedb292f4c23685adcd90c10c648
kier-ious <kier.diemer@gmail.com>
2023-05-25 16:14:57 -0700

    0-positive_or_negative.c

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Positive anything is better than negative nothing
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* random int between 0 and 2 */
+	return (0);
+}
