a472ccf4cc503500adc2cf75bb6ce6086e5c572b
kier-ious <kier.diemer@gmail.com>
2023-05-25 12:52:49 -0700

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,6 +6,10 @@
  */
 int main(void)
 {
-	printf ; sizeof % lu
+	printf("char: %zu 1 bytes\n")
+	printf("int: %zu 4 bytes\n")
+	printf("long int: %zu 4 bytes\n")
+	printf("long long int: %zu 8 bytes\n")
+	printf("float: %zu 4 bytes\n")
 	return (0);
 }
