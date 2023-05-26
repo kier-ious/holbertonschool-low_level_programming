e4369166bc28014e8b3df543459350d3d0ba9364
kier-ious <kier.diemer@gmail.com>
2023-05-26 14:25:36 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Ptints the aplhabet in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+		putchar(letter);
+
+		putchar('\n');
+
+	return (0);
+}
