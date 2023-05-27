59b4460146f9fb7ae122fc81ecda550926d0b0ef
kier-ious <kier.diemer@gmail.com>
2023-05-27 12:28:44 -0700

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -10,13 +10,13 @@
 
 	for (letter = 'a' ; letter <= 'z' ; letter++)
 
-		putchar (letter);
+	putchar (letter);
 
 	for (letter = 'A' ; letter <= 'Z' ; letter++)
 
-		putchar (letter);
+	putchar (letter);
 
-		putchar ('\n');
+	putchar ('\n');
 
 	return (0);
 
