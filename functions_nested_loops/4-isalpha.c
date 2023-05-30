6846609480616ce932e1664bd72b785cd1e95f4c
kier-ious <kier.diemer@gmail.com>
2023-05-29 21:13:08 -0700

    4-isalpha.c

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _isalpha - check the code.
- *
+ * _isalpha - controls is character is alphabetical.
+ * @c: character to be verified.
  * Return: return either a 1 or a 0
  */
 int _isalpha(int c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-	return (1);
+		return (1);
 
 	return (0);
 }
