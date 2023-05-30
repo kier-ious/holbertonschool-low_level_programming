b40a91772517cec83970d08cbaa3928dd66b50dd
kier-ious <kier.diemer@gmail.com>
2023-05-29 17:37:53 -0700

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - checking for lowercase
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	int r;
+
+	r = _islower('H');
+		_putchar(r + '0');
+
+	r = _islower('o');
+		_putchar(r + '0');
+
+	r = _islower(108);
+		_putchar(r + '0');
+		_putchar('\n');
+
+	return (0);
+
+}
