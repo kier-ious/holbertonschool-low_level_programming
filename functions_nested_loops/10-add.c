da7c4135e2c9828084596577ae34f3f81ed0cf72
kier-ious <kier.diemer@gmail.com>
2023-05-30 21:39:01 -0700

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - sums up two numbers
+ * @n: first integer
+ * @m: second integer
+ * Return: 0
+ */
+int add(int n, int m)
+{
+	_putchar('\n');
+
+	return (n + m);
+
+}
