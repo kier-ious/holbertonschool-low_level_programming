26fce3ee4670d23dc77404a67e6d7f659e7d2592
kier-ious <kier.diemer@gmail.com>
2023-05-31 20:53:22 -0700

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_number - print numbers 0 through 9
+ * Return: return numbers 0 through 9
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++);
+	putchar(x + '0');
+	putchar('\n');
+
+}
