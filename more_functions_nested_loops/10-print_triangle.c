a5d01ac0c7bea7089d3d595dc3b09f606212ab4f
kier-ious <kier.diemer@gmail.com>
2023-06-04 14:07:06 -0700

    10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -7,7 +7,7 @@
  */
 void print_triangle(int size)
 {
-	int x, y;
+	int x, y, z;
 
 	if (size <= 0)
 
@@ -15,13 +15,18 @@ void print_triangle(int size)
 		putchar('\n');
 	}
 	else
-
-	for (x = 0; x < size; x++)
 	{
-	for (y = 0; y < x; y++)
+		for (x = 0; x < size; x++)
+	{
+		for (y = 0; y < x; y++)
 		{
-		putchar(35);
+			putchar(32);
+		}
+		for (z = 0; z <= x; z++)
+		{
+			putchar(35);
 		}
 	putchar('\n');
+	}
 	}
 }
