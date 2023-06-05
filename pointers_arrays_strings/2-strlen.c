4f1a3290200038868476a11e8a06d581153dbcb8
kier-ious <kier.diemer@gmail.com>
2023-06-05 11:07:07 -0700

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - Write a function that returns the length of a string
+ * @*s: string to count
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+
+	return (c);
+}
