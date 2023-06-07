5a2bd5aa2f2a98e1bce779888df7b81574501ac6
kier-ious <kier.diemer@gmail.com>
2023-06-07 12:32:35 -0700

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,25 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * *_strcat - string to be added to
+ * _strcat - cats two strings
  * @dest: string to be added to
- * @src: stringt to be added
+ * @src: string to be added
  * Return: value of the cat strings
  */
 char *_strcat(char *dest, char *src)
 {
-	char *dest, *src;
+	int stringD, stringS;
 
-	while (src 
+	int stringD = 0;
+	int stringS = 0;
+
+	while (dest + stringD != '\0')
+		stringD++;
+	while (src + stringS != '\0' && stringD < 99)
+	{
+		stringD++;
+		stringS++;
+	}
+		(dest + stringD = src + stringS);
+		return (dest);
+}
