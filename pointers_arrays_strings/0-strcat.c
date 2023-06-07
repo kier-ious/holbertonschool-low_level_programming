1998f58a86ca4efcd55dd08f218d4fbb82ba6309
kier-ious <kier.diemer@gmail.com>
2023-06-07 09:47:06 -0700

    main.h

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strcat - string to be added to
+ * @dest: string to be added to
+ * @src: stringt to be added
+ * Return: value of the cat strings
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *dest, *src;
+
+	while (src 
