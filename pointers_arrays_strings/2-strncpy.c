0d5bfd20d996e740e855883ddcb0446eb95d9035
kier-ious <kier.diemer@gmail.com>
2023-06-08 14:34:10 -0700

    first

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -14,7 +14,7 @@ char *_strncpy(char *dest, char *src, int n)
 	for (i = 0; dest[i] != '\0'; i++)
 		;
 	for (j = 0; j < n && src[j] != '\0'; j++)
-		dest[i + j] = src[j];
+		dest[j] = src[j];
 	dest[i + j] = '\0';
 	return (dest);
 
