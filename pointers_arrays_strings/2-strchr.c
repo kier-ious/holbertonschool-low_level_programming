1e2a21dfb1a0981e44f9973259b0dcc598f3369a
kier-ious <kier.diemer@gmail.com>
2023-06-11 17:36:35 -0700

    Sloane

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strchr - locates a character in a string
+ * @s: source
+ * @c: character
+ * Return: located character
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != c)
+	{
+		if (*s == c)
+		{
+		return (s);
+		}
+		else
+			s++;
+	}
+	if (*s == c)
+
+		return (0);
+	else
+		return (0);
+}
