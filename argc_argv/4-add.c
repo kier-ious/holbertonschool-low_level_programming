9a07b969223095567eef800a2c1a2e3c1b4335b0
kier-ious <kier.diemer@gmail.com>
2023-06-20 17:22:20 -0700

    first push 4 4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - adds positive numnbers
+ * @argc: number of characters
+ * @argv: string of array
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int x = 0;
+	int y = 0;
+	int sum = 0;
+
+	if (argc < 1)
+
+	for (; x < argc; x++)
+	{
+		for (; argv[x][y] != '\0'; y++)
+		{
+			if (argv[x][y] < 48 || argv[x][y] > 57)
+
+				printf("Error\n");
+			return (1);
+
+		sum += atoi(argv[x]);
+		}
+	}
+}
