739947a4bad8aea9297b5e747b8c2c713806110a
kier-ious <kier.diemer@gmail.com>
2023-06-25 12:32:59 -0700

    annnnd again

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -22,7 +22,7 @@ int *array_range(int min, int max)
 		return (NULL);
 
 	/* intitialize elements of array */
-	for (i = min; i <= max; i++, min++)
+	for (i = 0; min <= max; i++, min++)
 		array[i] = min;
 
 	/* Return pointer to newly created array */
