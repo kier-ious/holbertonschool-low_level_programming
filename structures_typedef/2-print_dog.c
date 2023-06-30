c151c9727d7a96bcbe428658f9a0228b3e707456
kier-ious <kier.diemer@gmail.com>
2023-06-30 09:14:30 -0700

    final fix

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,18 +11,14 @@ void print_dog(struct dog *d)
 	{
 		if (d->name)
 			printf("Name: %s\n", d->name);
-
 		else
 			printf("Name: (nil)\n");
 		printf("Age: %.6f\n", d->age);
-
 		if (d->owner)
 			printf("owner: %s\n", d->owner);
-
 		else
 			printf("Owner: (nil)\n");
 	}
 	else
-
 		return;
 }
