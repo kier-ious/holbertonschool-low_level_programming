6797e34344788d8a96b609ad58fbe310d3bd52b9
kier-ious <kier.diemer@gmail.com>
2023-07-03 11:23:23 -0700

    .

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -7,7 +7,10 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	/* check if name and function ptr aren't NULL */
 	if (!name || !f)
+		/* return if they are NULL */
 		return;
+	/* call the function ptr w/ the name as the argument */
 	f(name);
 }
