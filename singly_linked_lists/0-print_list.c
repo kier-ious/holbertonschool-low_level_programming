07a8ab2c659e4f19bb757da715c799c768ed83ce
kier-ious <kier.diemer@gmail.com>
2023-07-13 11:13:27 -0700

    pushing edits of task 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_list - print list
+ * @h: a linked list
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t ct = 0;
+
+	for (; h != NULL; h = h->next)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		ct++;
+	}
+	return (ct);
+}
