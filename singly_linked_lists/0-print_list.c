618bdc8b58fdba6fcb210f1e4df9aedc58d7b3eb
kier-ious <kier.diemer@gmail.com>
2023-07-13 11:16:59 -0700

    made edits to betty errors

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,14 +10,14 @@ size_t print_list(const list_t *h)
 
 	for (; h != NULL; h = h->next)
 	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil\n");
-		}
-		else
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
+	if (h->str == NULL)
+	{
+		printf("[0] (nil\n");
+	}
+	else
+	{
+		printf("[%d] %s\n", h->len, h->str);
+	}
 		ct++;
 	}
 	return (ct);
