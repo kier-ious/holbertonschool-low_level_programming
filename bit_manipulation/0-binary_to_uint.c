7d7c7414c3e6bc835fe3981a0e25f0aa1750bdfa
kier-ious <kier.diemer@gmail.com>
2023-07-14 10:00:18 -0700

    finished task 0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * binary_to_uint - convert binary string to decimal
+ * @b: binary string
+ * Return: decimal (unsigned int)
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal = 0;
+	unsigned int i = 0;
+
+	for (; b[i] != '\0'; i++)
+	{
+		if (b[i] == '1')
+			decimal = (decimal << 1) | 1;
+		else if (b[i] == '0')
+			decimal <<= 1;
+		return (0);
+	}
+	return (decimal);
+}
---
+++ bit_manipulation/README.md
@@ -0,0 +1,4 @@
+C - Bit manipulation
+/**
+* Bit manipulation is the process of manipulating bits or groups of bits in a byte. Bit manipulation is often used to perform operations on data that are otherwise difficult or impossible to perform with traditional operator precedence.
+* /
