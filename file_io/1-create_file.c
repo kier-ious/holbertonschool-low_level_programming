654a6468fe1cddacb183e375d311f831b3923008
kier-ious <kier.diemer@gmail.com>
2023-07-16 14:11:20 -0700

    corrected spelling

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -3,7 +3,7 @@
  * create_file - creates a file with rw------- permissions
  * @filename: name given on saved file
  * @text_content: contents of the file. If NULL, create an empty file
- * Returns: 1 on success, -1 on failure
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
