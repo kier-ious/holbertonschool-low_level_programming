fabad81e08f846a9cd8915f7f704fc11d05c7af3
kier-ious <kier.diemer@gmail.com>
2023-07-19 14:47:38 -0700

    fixed while loop

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,20 +10,24 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *tmp, *new;
 
 	new = malloc(sizeof(dlistint_t)); /* allocate mem for new node */
-	if (!new) /* check if malloc was successful */
+	if (new == NULL) /* check if malloc was successful */
 		return (NULL);
+
 	new->n = n; /* set value of new node */
 	new->next = NULL; /* set next ptr of new node to NULL */
-	if (!*head) /* check if list is empty */
+
+	if (*head == NULL) /* check if list is empty */
 	{
-		new->prev = NULL; /* set prev ptr of new node to NULL */
-		*head = new; /* set head ptr to new node */
+		*head  = new; /* set prev ptr of new node to NULL */
 		return (*head); /* return new node */
 	}
+
 	tmp = *head; /* set tmp ptr to traverse thru list */
-	while (tmp->next) /* find last node in list */
+	while (tmp->next != NULL) /* find last node in list */
+	{
 		tmp = tmp->next;
+	}
 	tmp->next = new; /* set the ptr of that last node to the new node */
-	tmp->prev = *head; /* se prev ptr of new node to the current head */
-	return (tmp); /* last node (newest added) */
+	new->prev = tmp; /* se prev ptr of new node to the current head */
+	return (*head); /* last node (newest added) */
 }
